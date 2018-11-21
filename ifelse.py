fe30bdde0fa38f4b2580ed062cf9597ce023f612
Cameron Patterson <metacomet@camerons-air.resource.campus.njit.edu>
2018-11-21 15:29:10 -0500

    Fix: added idea file to .gitignore

---
+++ ifelse.py
@@ -0,0 +1,21 @@
+#Task
+#Given an integer, , perform the following conditional actions:
+#If  is odd, print Weird
+#If  is even and in the inclusive range of 2 to 5, print Not Weird
+#If  is even and in the inclusive range of  6 to 20 , print Weird
+#If  is even and greater than 20 , print Not Weird
+
+
+N = int(input())
+
+if N % 2 != 0:
+    print("Weird")
+
+if N % 2 == 0 and N in range(2,5):
+    print("Not Weird")
+
+if N % 2 == 0 and N in range(6,20):
+    print("Weird")
+
+if N % 2 == 0 and N > 20:
+    print("Not Weird")
